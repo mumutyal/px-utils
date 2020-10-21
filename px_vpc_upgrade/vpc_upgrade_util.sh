0dc187f439b91598c15d2de281d6cda71d9fc6b8
root <root@muraliVM1.fyre.ibm.com>
2020-10-21 04:15:22 -0700

    addressed the comments

--- px_vpc_upgrade/vpc_upgrade_util.sh
+++ px_vpc_upgrade/vpc_upgrade_util.sh
@@ -6,10 +6,33 @@ shopt -s expand_aliases
 alias ic="ibmcloud"
 
 [[ -z "$1" ]] && { echo "Cluster name is empty, specify a cluster name."; exit; }
+[[ -z "$2" ]] && { echo "Command name is empty. specify the command"; exit; }
 vol_ids=()
-#for ((argindex=2,index=0; argindex<=$#; argindex++,index++)); do
-#  vol_ids[index]=${!argindex}
-#done
+
+if [[ "$2" == "replace" || $2 == "upgrade" ]] ; then
+       command_name=$2
+else
+        echo "Usage: vpc_upgrade_util.sh  clustername command_name  workerid1 workerid2 ......"
+        exit 1
+fi
+
+if [ $#  -ge 3 ]; then
+  for ((argindex=3,index=0; argindex<=$#; argindex++,index++)); do
+	 echo "Worker id = $argindex"
+      WORKER_IDS[index]=${!argindex}
+  done
+else
+  echo "Worker ids are not specified upgrade/replace done for all workers"
+  WORKER_IDS=$(ic cs workers --cluster $CLUSTER $WORKER_POOL --json | jq -r '.[] | .id')
+fi
+
+echo "worker ids = ${WORKER_IDS[*]}"
+
+
+
+
+
+
 
 IFS='/' read -r -a CLUSTER_POOL <<< "$1"
 CLUSTER=${CLUSTER_POOL[0]}
@@ -25,73 +48,37 @@ echo "${CLUSTER_CHECK}"
 
 echo "Gathering information for cluster ${CLUSTER} ..."
 VPC_ID=$(ic cs cluster get --cluster $CLUSTER --json | jq -r '.vpcs[0]')
-WORKER_IDS=$(ic cs workers --cluster $CLUSTER $WORKER_POOL --json | jq -r '.[] | .id')
 CLUSTER_ID=$(ic cs cluster get --cluster ${CLUSTER} --json | jq -r '.id')
 JOB_COMMAND="/bin/systemctl restart  portworx;sleep 60"
+provisioning_worker_id=0
+
 
 waitforthenode () {
 
-DESIRED=1
-ALLREADY=0
-UPGRADE_STARTED=0
+DESIREDSTATE="deployed"
+ACTUALSTATE=""
 LIMIT=20
 SLEEP_TIME=60
-LONG_SLEEP_TIME=120
 
    ALLREADY=0
    repeat=0
 
-   oldifs="$IFS"
-   IFS=$'\n'
-   workers=($(ic cs workers --cluster $CLUSTER | grep $CLUSTER_ID)) 
-   worker_cnt=${#workers[@]}
-   while [ $repeat -lt $LIMIT ] && [ $UPGRADE_STARTED -ne $DESIRED ]; do
-     workers=($(ic cs workers --cluster $CLUSTER | grep $CLUSTER_ID)) 
-     for worker in "${workers[@]}"; do
-         worker_state=$(echo $worker | awk '{print $5}')
-         if [[ $worker_state != "Ready" ]]; then
-             echo "The upgrade/replace triggered... Waiting for the new workers provision...."
-             UPGRADE_STARTED=1             
-             break
-        else
-         echo "All workers are in ready state .... Upgrade is not yet started .. sleeping"
-     fi
-     done
-         sleep $SLEEP_TIME
-         repeat=$(( $repeat + 1 ))
-         if [ $repeat == $LIMIT ]; then
-           echo "Upgrade is not triggered from catalog ..Exiting ... Run the script again and trigger the upgrade from Dashboard"
-           exit 1
-         fi
-   done
-   sleep $LONG_SLEEP_TIME
+   worker_id=$1
    repeat=0
   ## Here wait for the 1 hour as the worker proviison takes long time
    LIMIT=90
-   #oldifs="$IFS"
-   #IFS=$'\n'
-   #workers=($(ic cs workers --cluster $CLUSTER | grep $CLUSTER_ID)) 
-   #IFS="$oldifs"
-   while [ $repeat -lt $LIMIT ] && [ $ALLREADY -ne $DESIRED ]; do
-     workers=($(ic cs workers --cluster $CLUSTER | grep $CLUSTER_ID)) 
-      worker_rdy_cnt=0
-    for worker in "${workers[@]}"; do
-      worker_state=$(echo $worker | awk '{print $5}')
-      if [[ $worker_state == "Ready" ]]; then
-         worker_rdy_cnt=$(( $worker_rdy_cnt + 1 ))
-         if  [ $worker_rdy_cnt -eq $worker_cnt ]; then
-            echo "Upgrade is done and All worker nodes are in ready state...."
-            ALLREADY=1
-            break
-         fi
+
+   while [ $repeat -lt $LIMIT ] && [ "$ACTUALSTATE" != "$DESIREDSTATE" ]; do
+      ACTUALSTATE=$(ic cs worker get --cluster   $CLUSTER  --worker $worker_id --json  | jq -r .lifecycle.actualState)
+      DESIREDSTATE=$(ic cs worker get --cluster   $CLUSTER  --worker $worker_id --json  | jq -r .lifecycle.desiredState)
+      if [[ $ACTUALSTATE == "Deployed" ]]; then
+            echo "New worker Upgrade/Replace  is done and All worker nodes are in ready state...."
        else
-         echo "One or more workers are not in ready state  Total no of workers :$worker_cnt.. Avialble workers :$worker_rdy_cnt.... waiting for new worker provision to complete"
-         sleep $SLEEP_TIME
-         repeat=$(( $repeat + 1 ))
+         echo "The new  worker: $worker_id still in proviisoning state.... waiting for new worker provision to complete"
       fi
-     done 
-   done
-    IFS="$oldifs"
+      sleep $SLEEP_TIME
+      repeat=$(( $repeat + 1 ))
+    done 
 }
 
 restartPortworxService () {
@@ -144,18 +131,14 @@ fi
 
 
 
-
-
-
-
 waitforportworxpods() {
-DESIRED=$(kubectl get ds/portworx -n kube-system -o json | jq .status.desiredNumberScheduled)
 RUNNING=0
 LIMIT=20
 SLEEP_TIME=30
-i=0
+DESIRED=$(kubectl get ds/portworx -n kube-system -o json | jq .status.desiredNumberScheduled)
+repeat=0
 
-while [ $i -lt $LIMIT ] && [ $DESIRED -ne $RUNNING ]; do 
+while [ $repeat -lt $LIMIT ] && [ $DESIRED -ne $RUNNING ]; do 
     RUNNING=$(kubectl get pods -n kube-system -l name=portworx --field-selector status.phase=Running -o json | jq '.items | length') 
     if [ $DESIRED -eq $RUNNING ]; then 
         echo "(Attempt $i of $LIMIT) Portworx pods: Desired $DESIRED, Running $RUNNING"
@@ -163,10 +146,71 @@ while [ $i -lt $LIMIT ] && [ $DESIRED -ne $RUNNING ]; do
         echo "(Attempt $i of $LIMIT) Portworx pods: Desired $DESIRED, Running $RUNNING, sleeping $SLEEP_TIME"
         sleep $SLEEP_TIME
     fi 
-    i=$(( $i + 1 ))
+    repeat=$(( $repeat + 1 ))
 done
 echo "All the pods moved to running state" 
 }
+
+waitfortheworkerdelete() {
+
+
+    NODE_DEPLOYING=0
+    DESIRED=1
+    LIMIT=20
+    SLEEP_TIME=60
+
+    repeat=0
+
+
+    #### retrive the new worker id 
+    while [ $NODE_DEPLOYING -ne $DESIRED  ] && [  $repeat -lt $LIMIT ]; do
+       WORKER_IDS=$(ic cs workers --cluster $CLUSTER $WORKER_POOL --json | jq -r '.[] | .id')
+       for id in ${WORKER_IDS}
+       do
+	   IFS='-' read -r -a WORKER_VALS <<< "$id"
+	   zone=$(ic cs worker get --worker $id --cluster $CLUSTER --json | jq -r .location)
+	   worker_state=$(ic cs worker get --cluster   $CLUSTER  --worker $id  --json | jq -r .lifecycle.actualState)
+	   if [[ $worker_state  == "deploying" ]]; then
+	        echo "New worker deployment started"
+		provisioning_worker_id=$id
+		NODE_DEPLOYING=1
+		break
+           else
+		echo "Worker deletion is in progress...sleeping"
+	   fi 
+       done
+        sleep $SLEEP_TIME
+         repeat=$(( $repeat + 1 ))
+         if [ $repeat == $LIMIT ]; then
+           echo "Upgrade/replace of  the worker taking too long ..Exiting ......."
+           exit 1
+         fi
+     done
+}
+
+
+executereplaceorupgrade () {
+    
+    waitfortheworkerdelete
+    waitforthenode  $provisioning_worker_id
+    waitforportworxpods
+
+    ##Check broken volumes and  reattch to the worker
+    index="0"
+    for vol_id in ${vol_ids[@]}; do
+    volume_attch_check=$(ic is vol ${vol_id} --json | jq -r '.volume_attachments[] .instance | .name')
+     if [ -z "$volume_attch_check" ]; then
+        echo "Volume: ${vol_id} is not attched to any node"
+        zone=$(ic cs worker get --worker $id --cluster $CLUSTER --json | jq -r .location) 
+        echo "Attaching the volume ....cluster $CLUSTER_ID worker ${id} volid ${vol_id}"
+        ic cs storage attachment create --cluster ${CLUSTER_ID} --worker ${provisioning_worker_id} --volume ${vol_id}
+        sleep 30
+        restartPortworxService $provisioning_worker_id
+    fi
+   done
+}
+         
+
 
 
 #####Before upgrade bring the volume ids using the worker id
@@ -174,44 +218,18 @@ volindex=0
 for id in ${WORKER_IDS}
 do
    IFS='-' read -r -a WORKER_VALS <<< "$id"
+   echo "worker id : $id"
    zone=$(ic cs worker get --worker $id --cluster $CLUSTER --json | jq -r .location)
    volid_perworker=$(ic is vols --json | jq -r --arg WORKER_NAME "$id" '.[]|select(.volume_attachments[] .instance.name==$WORKER_NAME) | .id')
    echo "volid :${volid_perworker[*]} is attched to the worker :${id}"
    vol_ids[volindex]=${volid_perworker[@]}
    ((volindex++))
+  sleep 20
+  ic cs worker $command_name  --cluster  $CLUSTER --worker $id
+  echo "The worker being deleted waiting for the new worker ............"
+  executereplaceorupgrade 
 done
 
-#### Wait for the Portowrx pods to become up and availble
 
-waitforthenode
-waitforportworxpods
-
-##Get all the volume ids in the cluster
-index="0"
-WORKER_IDS=$(ic cs workers --cluster $CLUSTER $WORKER_POOL --json | jq -r '.[] | .id')
-for vol_id in ${vol_ids[@]}; do
-  volume_attch_check=$(ic is vol ${vol_id} --json | jq -r '.volume_attachments[] .instance | .name')
-  if [ -z "$volume_attch_check" ]; then
-     echo "Volume: ${vol_id} is not attched to any node"
-      for id in ${WORKER_IDS}
-      do
-          IFS='-' read -r -a WORKER_VALS <<< "$id"
-          zone=$(ic cs worker get --worker $id --cluster $CLUSTER --json | jq -r .location) 
-          check_worker_attch=$(ic is vols --json | jq -r --arg WORKER_NAME "$id" '.[]|select(.volume_attachments[] .instance.name==$WORKER_NAME) | .id')
-          if [ -z "$check_worker_attch" ]; then
-             echo "Worker node is not attached to any Volume $id"
-             echo "Attaching the volume ....cluster $CLUSTER_ID worker ${id} volid ${vol_id}"
-             ic cs storage attachment create --cluster ${CLUSTER_ID} --worker ${id} --volume ${vol_id}
-             sleep 30
-             restartPortworxService $id
-             break
-           else
-             echo "Worker have volume attchements $id"
-          fi
-        done
-    else
-      echo "Volume : {$vol_id} is already attched"
-    fi
-done
          
 
